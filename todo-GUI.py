f2caa49c62b1a73cb53e1c5fcb5d090d534edecd
vinaydabhade30 <dsmlai4vinay2020@gmail.com>
2023-02-03 00:26:54 +0400

    implement time and theme

--- todo-GUI.py
+++ todo-GUI.py
@@ -1,6 +1,9 @@
 import function_mod
 import PySimpleGUI as sg
+import time
 
+sg.theme("black")
+clock = sg.Text("", key='clock')
 label = sg.Text("Type in to-do list")
 input_box = sg.InputText(tooltip="Enter todo", key="todo")
 add_button = sg.Button("Add")
@@ -13,16 +16,18 @@ exit_button = sg.Button("Exit")
 
 # create the window
 window = sg.Window("To-do List",
-                   layout=[[label],
+                   layout=[[clock],
+                           [label],
                            [input_box, add_button],
                            [list_box, edit_button, complete_button],
                            [exit_button]],
                    font=("calibre", 20))
 while True:
-    event, value = window.read()
-    print(1, event)
-    print(2, value)
-    print(3, value['r_list_todos'])
+    event, value = window.read(timeout=200)
+    window['clock'].update(value=time.strftime("%b %d, %Y %H:%M:%S"))
+    # print(1, event)
+    # print(2, value)
+    # print(3, value['r_list_todos'])
     match event:
         case 'Add':
             todos = function_mod.file_open_r()
@@ -31,24 +36,30 @@ while True:
             function_mod.file_open_w(todos)
             window['r_list_todos'].update(values=todos)
         case 'Edit':
-            todo_to_edit = value["r_list_todos"][0]
-            # print(todo_to_edit)
-            new_todo = value['todo']
-            todos = function_mod.file_open_r()
-            index = todos.index(todo_to_edit)
-            todos[index] = new_todo
-            function_mod.file_open_w(todos)
-            window['r_list_todos'].update(values=todos)
-            # print(index)
+            try:
+                todo_to_edit = value["r_list_todos"][0]
+                # print(todo_to_edit)
+                new_todo = value['todo']
+                todos = function_mod.file_open_r()
+                index = todos.index(todo_to_edit)
+                todos[index] = new_todo
+                function_mod.file_open_w(todos)
+                window['r_list_todos'].update(values=todos)
+                # print(index)
+            except IndexError:
+                sg.popup("Please select item first", font=('Helvetica', 20))
         case 'r_list_todos':
             window['todo'].update(value=value['r_list_todos'][0])
         case 'Complete':
-            todo_to_complete = value["r_list_todos"][0]
-            todos = function_mod.file_open_r()
-            todos.remove(todo_to_complete)
-            function_mod.file_open_w(todos)
-            window['r_list_todos'].update(values=todos)
-            window['todo'].update(value='')
+            try:
+                todo_to_complete = value["r_list_todos"][0]
+                todos = function_mod.file_open_r()
+                todos.remove(todo_to_complete)
+                function_mod.file_open_w(todos)
+                window['r_list_todos'].update(values=todos)
+                window['todo'].update(value='')
+            except IndexError:
+                sg.popup("Please select item first", font=('Helvetica', 20))
         case 'Exit':
             break
 
